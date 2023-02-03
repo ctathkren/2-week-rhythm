73b67056b6a0a2c4dc255a58344b7c81952c0feb
Alejandre Jose Ranoco San Pedro <ajrsanpedro215@gmail.com>
2023-02-03 18:22:03 +0800

    understood, refactored, and documented button code

--- HYMN/game/scenes/components/ref/arrow_button/ArrowButton.gd
+++ HYMN/game/scenes/components/ref/arrow_button/ArrowButton.gd
@@ -1,73 +1,139 @@
 extends AnimatedSprite
 
-var perfect = false
-var good = false
-var okay = false
+# VARIABLES
+# Button Identifier
+export var input = ""
+	# set in editor which key each button is assigned to
+	# MUST be exact key name in Project Settings>Input Map
+
+# Notes
+# Tracking
 var current_note = null
 
-export var input = ""
+# Feedback
+var hit_perfect = false
+var hit_good = false
+var hit_okay = false
+
+# Scoring
+const SCORE_PERFECT = 3
+const SCORE_GOOD = 2
+const SCORE_OKAY = 1
+const SCORE_MISS = 0
 
 
+# FUNCTIONS
 func _unhandled_input(event):
 	if event.is_action(input):
+
 		if event.is_action_pressed(input, false):
-			if current_note != null:
-				if perfect:
-					get_parent().increment_score(3)
-					current_note.destroy(3)
-				elif good:
-					get_parent().increment_score(2)
-					current_note.destroy(2)
-				elif okay:
-					get_parent().increment_score(1)
-					current_note.destroy(1)
-				_reset()
-			else:
-				get_parent().increment_score(0)
-		if event.is_action_pressed(input):
+			# registers all hits at the moment!
+			# consider: .is_action_just_pressed
+
+			"""
+			notes on 2nd argument of:
+			.is_action_pressed(action: String, exact: bool)
+
+			- if exact == false
+				- ignores additional input modifiers 
+				- for InputEventKey and InputEventMouseButton events
+				- and the direction for InputEventJoypadMotion events.
+			"""
+
+			hit_feedback()
+
 			frame = 1
+				# note: was under another if-statement with same condition
+				# except 2nd argument was not given
+
 		elif event.is_action_released(input):
+			# consider: .is_action_just_released
+
 			$PushTimer.start()
+			# recall: timer is for reseting animation frame
 
 
-func _on_PerfectArea_area_entered(area):
-	if area.is_in_group("note"):
-		perfect = true
+# ON KEY PRESS
+func hit_score_and_destroy(score):
+	get_parent().increment_score(score)
+
+	current_note.destroy(score)
 
 
-func _on_PerfectArea_area_exited(area):
-	if area.is_in_group("note"):
-		perfect = false
+func reset_note():
+	current_note = null
+
+	hit_perfect = false
+	hit_good = false
+	hit_okay = false
 
 
-func _on_GoodArea_area_entered(area):
-	if area.is_in_group("note"):
-		good = true
+func hit_feedback():
+	if not current_note == null:
+		# recall: current_note is controlled by if note is in/out of Okay Area
+
+		# .increment_score(score) called in Game.gd
+		# .destroy(score) called in Note.gd
+		
+		if hit_perfect:
+			hit_score_and_destroy(SCORE_PERFECT)
+		elif hit_good:
+			hit_score_and_destroy(SCORE_GOOD)
+		elif hit_okay:
+			hit_score_and_destroy(SCORE_OKAY)
+
+		reset_note()
+
+	else:
+		hit_score_and_destroy(SCORE_MISS)
 
 
-func _on_GoodArea_area_exited(area):
-	if area.is_in_group("note"):
-		good = false
+# SIGNALS
+
+# ANIMATION FRAME TIMER
+func _on_PushTimer_timeout():
+	# reverts animation to default
+	# reference uses 2-frame anim
+	# highly customizable for us
+	
+	frame = 0
 
 
+# NOTE CHECKING
+
+# Okay Areas
+# doubles as checker for considering if input should have an effect
+# ex. if in okay area, can hit; else not
 func _on_OkayArea_area_entered(area):
 	if area.is_in_group("note"):
-		okay = true
+		hit_okay = true
 		current_note = area
 
 
 func _on_OkayArea_area_exited(area):
 	if area.is_in_group("note"):
-		okay = false
+		hit_okay = false
 		current_note = null
 
 
-func _on_PushTimer_timeout():
-	frame = 0
+# Perfect Areas
+func _on_PerfectArea_area_entered(area):
+	if area.is_in_group("note"):
+		hit_perfect = true
 
 
-func _reset():
-	current_note = null
-	perfect = false
-	good = false
-	okay = false
+func _on_PerfectArea_area_exited(area):
+	if area.is_in_group("note"):
+		hit_perfect = false
+
+
+# Good Areas
+func _on_GoodArea_area_entered(area):
+	if area.is_in_group("note"):
+		hit_good = true
+
+
+func _on_GoodArea_area_exited(area):
+	if area.is_in_group("note"):
+		hit_good = false
+
