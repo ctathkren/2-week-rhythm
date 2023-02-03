5a85f5d8aabe4ca54c57538a28ebe5899960fe2b
04nada <37734971+04nada@users.noreply.github.com>
2023-02-04 02:13:14 +0800

    added functions for getting/settings note column

--- HYMN/game/scenes/components/note.gd
+++ HYMN/game/scenes/components/note.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
-var note_type = "note"
-var COLUMN_NUMBER
+var NOTE_TYPE = "note"
+var column_number = 1
 
 # not sure if we'll use these variables if our timing is Area2D-based
 var correct_hit_time
@@ -11,27 +11,53 @@ var judgement = "do_nothing"
 var SCROLL_SPEED = 25 # just to make "scroll speed" itself a smaller number
 var SCROLL_SPEED_MULTIPLIER = 20
 
+var partner_note
+var partner_note_bar
+
 # ---
 
 func _ready():
 	pass
 
 func _physics_process(delta):
+	updateNoteSprite()
+	
 	# move down by SCROLL_SPEED * delta * whatever mess is caused by the highway parallax
 	# also move to the left/right based on the parallax
 	position.y += SCROLL_SPEED * SCROLL_SPEED_MULTIPLIER * delta	
 	pass
 
 # ---
+func updateNoteSprite():
+	var animation_name = ""
+	
+	if column_number > 0:
+		animation_name += "growth"
+	elif column_number < 0:
+		animation_name += "decay"
+	
+	animation_name += String(abs(column_number))
+	
+	$NoteSprite.play(animation_name)
+	$NoteGlowSprite.play(animation_name)
 
 func getNoteType():
-	return note_type
+	return NOTE_TYPE
+	
+func getColumnNumber():
+	return column_number
+	
+func setColumnNumber(col):
+	column_number = col
+	updateNoteSprite()
 
 func getJudgement():
 	return judgement
 	
 func setJudgement(j):
 	judgement = j
+
+# ---
 
 func beHit():
 	# fade out
