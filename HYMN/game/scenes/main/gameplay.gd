04228b1783bfc33eab2d8c53c53657cc17ad898a
Alejandre Jose Ranoco San Pedro <ajrsanpedro215@gmail.com>
2023-02-03 09:11:16 +0800

    fixed bugs

---
+++ HYMN/game/scenes/main/gameplay.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var BPM
+
+# ---
+
+func _ready():
+	pass
+
+func _physics_process(_delta):
+	if Input.is_action_just_pressed("ui_down"):
+		resetLevel()
+
+# ---
+
+func resetLevel():
+	var _reload = get_tree().reload_current_scene()
