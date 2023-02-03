1762a1123e34f113e702131cc6e213164d356430
Alejandre Jose Ranoco San Pedro <ajrsanpedro215@gmail.com>
2023-02-03 09:51:56 +0800

    file structure updates

--- HYMN/game/scenes/main/2d/gameplay.gd
+++ HYMN/game/scenes/main/2d/gameplay.gd
@@ -4,11 +4,15 @@ var BPM
 
 # ---
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(_delta):
+	if Input.is_action_just_pressed("ui_down"):
+		resetLevel()
+
+# ---
+
+func resetLevel():
+	var _reload = get_tree().reload_current_scene()
