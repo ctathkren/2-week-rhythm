c7b230560c1b6f8f01a47c6a322cff71075a75f9
04nada <37734971+04nada@users.noreply.github.com>
2023-02-05 03:55:07 +0800

    partly fixed function naming scheme

--- HYMN/game/scenes/main/lawrence/2d/gameplay_orthogonal.gd
+++ HYMN/game/scenes/main/lawrence/2d/gameplay_orthogonal.gd
@@ -5,23 +5,23 @@ var BPM
 # ---
 
 func _ready():
-	$Note2.setColumnNumber(1)
-	$Note2.setColumnNumber(3)
-	$Note3.setColumnNumber(2)
-	$Note4.setColumnNumber(-1)
-	$Note5.setColumnNumber(-2)
-	$Note6.setColumnNumber(-3)
-	$Note7.setColumnNumber(-3)
-	$Note8.setColumnNumber(-2)
-	$Note9.setColumnNumber(3)
+	$Note2.set_column_number(1)
+	$Note2.set_column_number(3)
+	$Note3.set_column_number(2)
+	$Note4.set_column_number(-1)
+	$Note5.set_column_number(-2)
+	$Note6.set_column_number(-3)
+	$Note7.set_column_number(-3)
+	$Note8.set_column_number(-2)
+	$Note9.set_column_number(3)
 
 
 func _physics_process(_delta):
 	if Input.is_action_just_pressed("ui_down"):
-		resetLevel()
+		reset_level()
 	
 
 # ---
 
-func resetLevel():
+func reset_level():
 	var _reload = get_tree().reload_current_scene()
