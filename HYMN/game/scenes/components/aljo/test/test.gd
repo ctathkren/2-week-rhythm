172a584d55ccbbd204a5732c5ff87e9ecbb24556
Alejandre Jose Ranoco San Pedro <ajrsanpedro215@gmail.com>
2023-02-05 17:07:30 +0800

    tested note animation handling

--- HYMN/game/scenes/components/aljo/test/test.gd
+++ HYMN/game/scenes/components/aljo/test/test.gd
@@ -1,46 +1,8 @@
 extends Node2D
 
-var hit_perfect = true
-var hit_good    = true
-var hit_okay    = true
-
-const SCORE_PERFECT = 3
-const SCORE_GOOD    = 2
-const SCORE_OKAY    = 1
-const SCORE_MISS    = 0
-
-var hit_feedbacks_to_score = {
-	hit_perfect : SCORE_PERFECT,
-	hit_good : SCORE_GOOD,
-	hit_okay : SCORE_OKAY,
-}
-
-
 func _ready():
-	print(hit_feedbacks_to_score.size())
+    $SpritesButton.animation = "decay"
+    $SpritesButton.frame = 3
 
-
-"""
-func _ready():
-	hit_feedback()
-
-
-func hit_feedback():
-	for i in range(len(hit_feedbacks_to_score)):
-		if feedback:
-			hit_score_and_destroy(hit_feedbacks_to_score[feedback])
-
-	
-	if hit_perfect:
-		hit_score_and_destroy(SCORE_PERFECT)
-	elif hit_good:
-		hit_score_and_destroy(SCORE_GOOD)
-	elif hit_okay:
-		hit_score_and_destroy(SCORE_OKAY)
-	
-
-
-func hit_score_and_destroy(score):
-	print('Arguments passed: ' + str(score))
-
-"""
+    $SpritesGlow.animation = "decay"
+    $SpritesGlow.frame = 3
