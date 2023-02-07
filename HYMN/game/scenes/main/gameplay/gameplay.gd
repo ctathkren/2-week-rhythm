aaa2759cc746a107413eff3eef6dada73dc84b26
Alejandre Jose Ranoco San Pedro <ajrsanpedro215@gmail.com>
2023-02-07 23:04:22 +0800

    pass level info from level select to gameplay

--- HYMN/game/scenes/main/gameplay/gameplay.gd
+++ HYMN/game/scenes/main/gameplay/gameplay.gd
@@ -108,6 +108,14 @@ func _input(event):
 
 # ON READY
 func _ready():
+	# Set Level
+	if Global.level == "growth":
+		$Backgrounds/Growth.visible = true
+		$Backgrounds/Decay.visible = false
+	elif Global.level == "decay":
+		$Backgrounds/Growth.visible = false
+		$Backgrounds/Decay.visible = true
+
 	# reference uses random lanes for note spawning
 	#randomize()
 	pass
