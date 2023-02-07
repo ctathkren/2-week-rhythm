bb5b6d6844a842876e1a233cb6f3c7c8dada688a
Alejandre Jose Ranoco San Pedro <ajrsanpedro215@gmail.com>
2023-02-07 21:47:22 +0800

    remove options

--- HYMN/ui/scenes/main/title_screen/title_screen.gd
+++ HYMN/ui/scenes/main/title_screen/title_screen.gd
@@ -2,16 +2,12 @@ extends Control
 
 # VARIABLES
 const LEVEL_SELECT_PATH = "res://ui/scenes/main/title_screen/level_select/level_select.tscn"
-const OPTIONS_PATH = "res://ui/scenes/main/title_screen/options/options.tscn"
 const CREDITS_PATH = "res://ui/scenes/main/title_screen/credits/credits.tscn"
 
 
 # SIGNALS
 func _on_LevelSelectButton_pressed():
 	change_scene(LEVEL_SELECT_PATH)
-
-func _on_OptionsButton_pressed():
-	change_scene(OPTIONS_PATH)
 
 func _on_CreditsButton_pressed():
 	change_scene(CREDITS_PATH)
