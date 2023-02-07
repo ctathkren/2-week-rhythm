658e993063a6c3f80ec8dd65693fb6237e4cfee9
04nada <37734971+04nada@users.noreply.github.com>
2023-02-08 01:58:20 +0800

    added functions for reading, loading, and sending level data from the levels directory

--- HYMN/game/scenes/main/gameplay/gameplay.gd
+++ HYMN/game/scenes/main/gameplay/gameplay.gd
@@ -1,6 +1,30 @@
 extends Node2D
 
-# VARIABLES
+### VARIABLES
+
+# Level Info from File
+const LEVEL_INFO_FILENAME = "level.hymn"
+
+onready var gameplay_3d_node = $GameplayPerspectivized/Gameplay3DViewport/Gameplay3D
+
+# Gameplay UI Elements
+onready var combo_label = $UI/ComboLabel
+onready var score_label = $UI/ScoreLabel
+
+var feedback_label
+var feedback_label_timer
+
+onready var feedback_growth_label = $UI/FeedbackGrowthLabel
+onready var feedback_growth_label_timer = $UI/FeedbackGrowthLabel/FeedbackVisibleTimer
+onready var feedback_decay_label = $UI/FeedbackDecayLabel
+onready var feedback_decay_label_timer = $UI/FeedbackDecayLabel/FeedbackVisibleTimer
+
+const TEXT_MISS = "MISS"
+const TEXT_GOOD = "GOOD"
+const TEXT_PERFECT = "PERFECT!"
+
+#const COLOR_GROWTH = "eb8f54"
+#const COLOR_DECAY  = "393ea2"
 
 # Scoring & Feedback
 var score_stats = {
@@ -37,31 +61,9 @@ var score_stats = {
 	}
 }
 
-var level_current_measure
+# --- -----
 
-onready var combo_label = $UI/ComboLabel
-onready var score_label = $UI/ScoreLabel
-
-var feedback_label
-var feedback_label_timer
-
-onready var feedback_growth_label = $UI/FeedbackGrowthLabel
-onready var feedback_growth_label_timer = $UI/FeedbackGrowthLabel/FeedbackVisibleTimer
-onready var feedback_decay_label = $UI/FeedbackDecayLabel
-onready var feedback_decay_label_timer = $UI/FeedbackDecayLabel/FeedbackVisibleTimer
-
-const TEXT_MISS = "MISS"
-const TEXT_GOOD = "GOOD"
-const TEXT_PERFECT = "PERFECT!"
-
-#const COLOR_GROWTH = "eb8f54"
-#const COLOR_DECAY  = "393ea2"
-
-
-
-
-
-# FUNCTIONS
+### FUNCTIONS
 
 # LOOPS
 
@@ -76,10 +78,29 @@ func _input(event):
 
 # ON READY
 func _ready():
-	pass
+	load_level("res://levels/Level2")
+
+func load_level(level_folder_path):
+	var level_info_file_path = level_folder_path
 	
-func load_level(level_file):
-	pass
+	if level_info_file_path[-1] != '/':
+		level_info_file_path += "/"
+		
+	level_info_file_path += LEVEL_INFO_FILENAME
+	
+	# ---
+	
+	var level_config = ConfigFile.new()
+	
+	var err = level_config.load(level_info_file_path)
+	if err != OK:
+		printerr("Failed to load level from path: \"" + level_info_file_path + "\".")
+		return
+		
+	for key in level_config.get_section_keys("LevelInfo"):
+		Global.level_info[key] = level_config.get_value("LevelInfo", key)
+	
+	gameplay_3d_node.fetch_level_notes()
 	
 # ---
 
@@ -119,7 +140,7 @@ func count_hit_feedback(input_type, score_to_add):
 	score_stats.combined.score_feedback_to_button_hit_feedback[score_to_add] += 1
 
 func update_score_label():
-	score_label.text = str(score_stats.combined.score)
+	score_label.text = "SCORE: " + str(score_stats.combined.score)
 
 func update_combo_label():
 	if score_stats.combined.combo > 0:
@@ -146,21 +167,7 @@ func update_feedback_label(input_type, score_to_add):
 		feedback_label.text = TEXT_MISS
 
 	# timer
-	var measure = level_current_measure
-	var notes
-	
-	if measure == 1:
-		notes = spawn_notes_measure_1
-	elif measure == 2:
-		notes = spawn_notes_measure_2
-	elif measure == 3:
-		notes = spawn_notes_measure_3
-	elif measure == 4:
-		notes = spawn_notes_measure_4
-
-	# resolve zero-division error
-	if notes == 0:
-		notes = 1
+	var seconds_per_beat = 60.0 / Global.level_info.bpm
 	
 	feedback_label_timer.wait_time = (seconds_per_beat / 2) - (0.025)
 	feedback_label_timer.start()
@@ -179,8 +186,9 @@ func increment_score(input_type, score_to_add):
 	update_combo_label()
 	update_feedback_label(input_type, score_to_add)
 
+# --- -----
 
-# SIGNALS
+### SIGNALS
 
 func _on_Gameplay3D_score_incremented(input_type, score_to_add):
 	increment_score(input_type, score_to_add)
