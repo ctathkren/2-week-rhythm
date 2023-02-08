4084cecd1b5f54462c9d73b2fa7935cfb38d37a6
Alejandre Jose Ranoco San Pedro <ajrsanpedro215@gmail.com>
2023-02-08 10:37:58 +0800

    from lawrence

--- HYMN/game/scenes/components/button/button.gd
+++ HYMN/game/scenes/components/button/button.gd
@@ -16,13 +16,13 @@ var hittable_notes = []
 var hit_feedbacks = {
 	"hit_perfect": false,
 	"hit_good": false,
-	"hit_miss": false
+	"hit_miss": false,
 }
 
 var hit_feedback_areas = {
 	"hit_perfect": $PerfectArea,
 	"hit_good": $GoodArea,
-	"hit_miss": $EarlyMissArea
+	"hit_miss": $EarlyMissArea,
 }
 
 signal score_incremented(input_type, score_to_add)
