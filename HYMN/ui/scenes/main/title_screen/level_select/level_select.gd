ef59858fd606c353ed215cbc4d3d98e7181d6109
Alejandre Jose Ranoco San Pedro <ajrsanpedro215@gmail.com>
2023-02-08 20:42:03 +0800

    title screen, level select, and credits

--- HYMN/ui/scenes/main/title_screen/level_select/level_select.gd
+++ HYMN/ui/scenes/main/title_screen/level_select/level_select.gd
@@ -6,16 +6,24 @@ export var growth_passed := false
 const LEVEL_PATH = "res://game/scenes/main/gameplay/gameplay.tscn"
 const BACK_PATH = "res://ui/scenes/main/title_screen/title_screen.tscn"
 
+const DEFAULT_ROTATE_SPEED = 15
+const GROWTH_ROTATE_SPEED = 60
+const DECAY_ROTATE_SPEED = 90
+var bunnies_rotate_speed := DEFAULT_ROTATE_SPEED
 
 # MAIN FUNCTIONS
 # Testing Decay Unlock
 func _ready():
-	growth_passed = Global.growth_passed
+	Global.growth_passed = growth_passed
 
 	if Global.growth_passed:
-		decay_text("Decay")
+		decay_text_unlocked()
 	else:
-		decay_text("Locked")
+		decay_text_locked()
+
+
+func _process(delta):
+	$Bunnies.rect_rotation -= bunnies_rotate_speed * delta
 
 
 # SIGNALS
@@ -25,6 +33,7 @@ func _on_GrowthButton_mouse_entered():
 	decay_off()
 
 	growth_on()
+	
 
 func _on_GrowthButton_mouse_exited():
 	growth_off()
@@ -45,12 +54,12 @@ func _on_DecayButton_mouse_entered():
 
 	# Decay On
 	decay_on()
-	decay_text("Decay")
+	decay_text_unlocked()
 		
 func _on_DecayButton_mouse_exited():
 	decay_off()
 	if not Global.growth_passed:
-		decay_text("Locked")
+		decay_text_locked()
 
 	default_on()
 
@@ -68,6 +77,8 @@ func _on_BackButton_pressed():
 func default_on():
 	$Backgrounds/Default.visible = true
 	$Music/Default.play()
+
+	bunnies_rotate_speed = DEFAULT_ROTATE_SPEED
 func default_off():
 	$Backgrounds/Default.visible = false
 	$Music/Default.stop()
@@ -75,6 +86,8 @@ func default_off():
 func growth_on():
 	$Backgrounds/Growth.visible = true
 	$Music/Growth.play()
+
+	bunnies_rotate_speed = GROWTH_ROTATE_SPEED
 func growth_off():
 	$Backgrounds/Growth.visible = false
 	$Music/Growth.stop()
@@ -82,11 +95,30 @@ func growth_off():
 func decay_on():
 	$Backgrounds/Decay.visible = true
 	$Music/Decay.play()
+
+	bunnies_rotate_speed = DECAY_ROTATE_SPEED
 func decay_off():
 	$Backgrounds/Decay.visible = false
 	$Music/Decay.stop()
-func decay_text(text):
-	$Content/VBoxContainer/Buttons/Decay/DecayButton.text = str(text)
+func decay_text_unlocked():
+	# hide
+	$Buttons/DecayButton.text = ""
+
+	# show
+	$Buttons/DecayButton/SongTitleLabel.visible = true
+	$Buttons/DecayButton/SongTitleLabel.text = "Decay"
+
+	$Buttons/DecayButton/ArtistLabel.visible = true
+	$Buttons/DecayButton/ArtistLabel.text = "by Raco"
+
+	$Buttons/DecayButton/VineSprite2.visible = false
+func decay_text_locked():
+	# hide
+	$Buttons/DecayButton/SongTitleLabel.visible = false
+	$Buttons/DecayButton/ArtistLabel.visible = false
+
+	# show
+	$Buttons/DecayButton.text = "Locked"
 
 func change_scene(scene):
 	var _change_scene = get_tree().change_scene(scene)
