af50a2dcc160d8d71afd5cc7cabb70b9c873d3c0
04nada <37734971+04nada@users.noreply.github.com>
2023-02-09 15:23:28 +0800

    unused

--- HYMN/ui/scenes/main/level/game_end_scene/game_end_scene.gd
+++ HYMN/ui/scenes/main/level/game_end_scene/game_end_scene.gd
@@ -10,9 +10,9 @@ func _on_CreditsButton_pressed():
 	change_scene(CREDITS_PATH)
 
 func _on_TitleScreenButton_pressed():
-    change_scene(TITLE_SCREEN_PATH)
+	change_scene(TITLE_SCREEN_PATH)
 
 
 # HELPER FUNCTIONS
 func change_scene(scene):
-    var _change_scene = get_tree().change_scene(scene)
+	var _change_scene = get_tree().change_scene(scene)
